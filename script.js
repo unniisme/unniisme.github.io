db773c0bda2a7dd780265b770459adc44e7677fd
unniisme <122001019@smail.iitpkd.ac.in>
2022-12-04 14:39:59 +0530

    Cool ribbon thing

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const points_n = 12;
 var m_x=0, m_y=0;
-var x=100, y=100, vx=0, vy=0, dt=0.1, k=10, mu=1;
-var Xs = new Array(points_n), Ys = new Array(points_n);
-var vXs = new Array(points_n), vYs = new Array(points_n);
-var ks = new Array(points_n);
+
+//Grid
+const k=10, q=-5000, mu=1, dt=0.1;
+const gridUnit = 5;
+var height = 20, length = 20;
+var positions = new Array(length);
+var anchors = new Array(length);
+var velocities = new Array(length);
 
 function main() {
   init();
@@ -13,33 +17,65 @@ function main() {
 
 function init()
 {
-  for(let i=0; i<points_n; i++)
+  // Set canvas width to window width
+  canvas.width  = window.innerWidth;
+  var rect = canvas.getBoundingClientRect();
+
+  //Initialize grid
+  const offset = {x:0, y:500};
+  height = 5;
+  length = Math.round(rect.width/gridUnit);
+  
+  for (let i=0; i<length; i++)
   {
-    Xs[i] = i*100;
-    Ys[i] = i*100;
-    vXs[i] = 0;
-    vYs[i] = 0;
-    ks[i] = i;
+    positions[i] = new Array(height);
+    anchors[i] = new Array(height);
+    velocities[i] = new Array(height);
+
+    for (let j=0; j<height; j++)
+    {
+      positions[i][j] = {x : i*gridUnit + offset.x, y : j*gridUnit + offset.y};
+      anchors[i][j] = {x : i*gridUnit + offset.x, y : j*gridUnit + offset.y};
+      velocities[i][j] = {x : 0, y : 0};
+    }  
   }
+}
+
+function updatePoint(i,j)
+{
+
+  //Update grid
+  // All maths
+  let x = m_x-positions[i][j].x;
+  let y = m_y-positions[i][j].y;
+  let ax = q*x/(x*x+y*y);
+  let ay = q*y/(x*x+y*y);
+
+  x = anchors[i][j].x-positions[i][j].x;
+  y = anchors[i][j].y-positions[i][j].y;
+
+  ax += k*x - mu*velocities[i][j].x;
+  ay += k*y - mu*velocities[i][j].y;
+
+  velocities[i][j].x += ax*dt;
+  velocities[i][j].y += ay*dt;
+
+  positions[i][j].x += velocities[i][j].x*dt;
+  positions[i][j].y += velocities[i][j].y*dt;
+
 }
 
 function update()
 {
-  vx += (k*(m_x-x) - vx*mu)*dt
-  vy += (k*(m_y-y) - vy*mu)*dt
-
-  x += vx*dt;
-  y += vy*dt;
-
-  for (let i=0; i<points_n; i++)
+  //Update each point
+  for (let i=0; i<length; i++)
   {
-    vXs[i] += (ks[i]*(m_x-Xs[i]) - vXs[i]*mu)*dt
-    vYs[i] += (ks[i]*(m_y-Ys[i]) - vYs[i]*mu)*dt
 
-    Xs[i] += vXs[i]*dt;
-    Ys[i] += vYs[i]*dt;
-  }
-
+    for (let j=0; j<height; j++)
+    {
+      updatePoint(i,j);
+    }  
+  }  
   draw();
 }
 
@@ -50,23 +86,25 @@ function draw()
   ctx.clearRect(0, 0, rect.width, rect.height); // clear canvas
 
 
-  let radius = 5;
-  // ctx.fillStyle = "rgb(0, 200, 0)";
+  let radius = 2;
   // ctx.beginPath();
   // ctx.arc(x - rect.left, y - rect.top, radius, 0, 2 * Math.PI, false);
   // ctx.fill();
-
-  for (let i=0; i<points_n; i++)
+  
+  // Draw
+  for (let i=0; i<length; i++)
   {
-    ctx.fillStyle = "rgb(" + String(i*18) +", 200, 0)";
-
-    ctx.beginPath();
-    ctx.arc(Xs[i] - rect.left, Ys[i] - rect.top, radius, 0, 2 * Math.PI, false);
-    ctx.fill();
+    for (let j=0; j<height; j++)
+    {
+      let dist = (positions[i][j].x - anchors[i][j].x)*(positions[i][j].x - anchors[i][j].x) + (positions[i][j].y - anchors[i][j].y)*(positions[i][j].y - anchors[i][j].y)
+      ctx.fillStyle = "rgba(0, 200, " + String(dist/2) + ", " + String(dist/200) + ")";
+      ctx.beginPath();
+      ctx.arc(positions[i][j].x - rect.left, positions[i][j].y - rect.top, radius, 0, 2 * Math.PI, false);
+      ctx.fill();
+    }  
   }
 
-
-  window.requestAnimationFrame(update);
+  window.requestAnimationFrame(update); // Next frame
 }
 
 function coordinate(event) 
